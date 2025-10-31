6ec0c19156c249609159ed73017be2cb8286a969
lastwolf <lastwolf1919@gmail.com>
2025-10-31 18:47:20 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T15:41:02.317Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-10-31T15:47:19.341Z
+Commit: Fix broken links in documentation
